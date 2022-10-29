ef721b94357bf0249d8221964438065347021f36
RodolfoZahir <zahirvlogs121101@gmail.com>
2022-10-28 19:34:18 -0500

    Nuevos cambios

---
+++ Parcial 2/FETCH/FETCH.js
@@ -0,0 +1,15 @@
+const btnllamar = document.getElementById('btnLlamar');
+const apiData = document.getElementById('apiData');
+var image = document.querySelector('img');
+
+const llamarfto = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(respuesta => respuesta.json())
+    .then(data => {
+        console.log(data);
+        image.src =data.message;
+    })
+}
+
+btnllamar.addEventListener('click', llamarfto);
+
