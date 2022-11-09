816032cbb1cf0861de8810eaa80b4bdc953fa4ac
RodolfoZahir <zahirvlogs121101@gmail.com>
2022-11-09 17:03:55 -0600

    Nuevos cambios

---
+++ Parcial 3/ModuloEcma.js
@@ -0,0 +1,24 @@
+export function mayuscula(palabra) {
+    return palabra.replace('a','o');
+}
+export function longitud(palabra) {
+    return palabra.lenght; 
+}
+
+/*
+Pruebas
+let number = 42;
+const hello = () => "Hello!";
+const goodbye = () => "¡Adiós!";
+
+export { number };                   // Se crea un módulo y se añade number
+export { hello, goodbye as bye };    // Se añade saludar y despedir al módulo
+export { hello as greet };           // Se añade otroNombre al módulo
+
+export function numeromagico(num1) {
+}
+export function numerodesuerte(num1) {
+    var num1 = 7;
+    return num1();
+}
+*/
