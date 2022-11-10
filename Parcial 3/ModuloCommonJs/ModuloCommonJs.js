dd064ad6db4b8f97dba1666683d914f85dde959c
RodolfoZahir <zahirvlogs121101@gmail.com>
2022-11-09 19:01:32 -0600

    Nuevos cambios

---
+++ Parcial 3/ModuloCommonJs/ModuloCommonJs.js
@@ -0,0 +1,12 @@
+var cowsayjs = require("cowsayjs");
+
+console.log(cowsayjs.moo("Rodolfo Zahir Assam Méndez"));
+/*
+var cowsay = require("cowsay");
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+// or cowsay.think()
+*/
