0aa56f99b17efb500d12a8dacc883ed6e9edc49f
RodolfoZahir <zahirvlogs121101@gmail.com>
2022-12-02 17:28:19 -0600

    Nuevos cambios

--- Parcial 3/FormularioConsulta/Formulario.php
+++ Parcial 3/FormularioConsulta/Formulario.php
@@ -1,16 +1,16 @@
 <?php
-$id = $_POST['idProducto']; //Hola
+$id = $_POST['idinformacion']; //Hola
 $servidor = "localhost";
-$basedatos = "cliente";
+$basedatos = "informacion";
 $usuario = "root";
 $password = "rodozahir123";
 
 $con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die ("No se pudo conectar a localhost");
-$consulta = "select * from libro where idProducto=$id";
+$consulta = "select * from tienda where idinformacion=$id";
 $registros = mysqli_query($con,$consulta) or die ("Problemas en el select");
 
 //Resultado como array asociativo
-$result = mysqli_fetch_all($registros,MYSQLI_ASSOC);
+$result = mysqli_fetch_all($registros);
 mysqli_close($con);
 echo json_encode($result);
 ?>
---
+++ Parcial 3/Sesiones/1DeDiciembre.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$user = $_POST['usuario'];
+$passw = $_POST['password'];
+
+if(isset($_SESSION['usuario'])){
+    printf("REANUDANDO SESION <br>");
+    printf("Bienvenido de nuevo".$_SESSION['usuario']);
+} else {
+    $_SESSION['usuario']=$user;
+    printf("NUEVA SESION <br>");
+    printf("Bienvenido %s",$user);
+}
+?>
